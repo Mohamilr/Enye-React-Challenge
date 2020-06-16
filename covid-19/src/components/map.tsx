bb4023cb7f93ebd5f7fb1bf6361e25b061b70253
mohamilr <ibrahimdamy7@gmail.com>
2020-06-16 16:52:01 +0100

    use leaflet map

--- covid-19/src/components/map.tsx
+++ covid-19/src/components/map.tsx
@@ -1,53 +1,340 @@
 import React, { useState, useEffect } from 'react';
-import { InputNumber, Col, Descriptions } from 'antd';
-import 'antd/dist/antd.css';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import TextField from '@material-ui/core/TextField'
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import { Map, TileLayer } from 'react-leaflet';
+import Icon from '@material-ui/core/Icon';
+import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 
-type CustomValue = any;
-export interface Result {
-    address: string,
-    distance: number,
-    id: string,
-    name: string,
-    phone_number: string,
-}
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        flex: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: '2em'
+        },
+        root: {
+            padding: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 400,
+        },
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1,
+        },
+        iconButton: {
+            padding: 10,
+        },
+        divider: {
+            height: 28,
+            margin: 4,
+        },
+    }),
+);
 
-export interface Array {
-
-}
-
-const Map = () => {
-    // const [location, setLocation] = useState<CustomValue>([]);
-    // const [searchKey, setSearchKey] = useState<string>('');
-    const [radius, setRadius] = useState<string>('24');
-    const [result, setResult] = useState<Result[]>([]);
-
-    useEffect(() => {
-        handleSearch()
-        // handleLocation();
-    }, [radius]);
-
-    //     const handleLocation = async () => {
-    //         const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/lagos.json?access_token=pk.eyJ1IjoibW9oYW1pbHIiLCJhIjoiY2tiMm42eW81MDBvNDJ4anp2YW5xY3RkeiJ9.K5J-TRV3i3LRkK-RwWEqKw`, {
-    //             method: 'GET'
-    //         });
-
-    //         const data = await response.json()
-    //         console.log('place', data)
-    //         for (let i=0; i < data.features.length; i++ ) {
-    //             if (i === 0) {
-    //                 setLocation(data.features[i].bbox)
-    //             }
-    //         }
+const Maps = () => {
+    const classes = useStyles();
+    const [location, setLocation] = useState<string>('');
+    const [lat, setLat] = useState<number>(6.5243793);
+    const [lng, setLng] = useState<number>(3.3792057);
+    const [searchKey, setSearchKey] = useState<string>('');
+    const [radius, setRadius] = useState<string>('12');
+    // const [positionA, setPositionA] = useState([]);
+    // const [positionB, setPositionB] = useState([]);
 
 
-    //     }
-    // console.log('filter', location[0])
+    const handlePlace = async () => {
+        const response = await fetch(`https://spott.p.rapidapi.com/places/autocomplete?q=${searchKey}&limit=1&skip=0&language=en&country=AF,
+        AX,
+        AL,
+        DZ,
+        AS,
+        AD,
+        AO,
+        AI,
+        AQ,
+        AG,
+        AR,
+        AM,
+        AW,
+        AU,
+        AT,
+        AZ,
+        BH,
+        BS,
+        BD,
+        BB,
+        BY,
+        BE,
+        BZ,
+        BJ,
+        BM,
+        BT,
+        BO,
+        BQ,
+        BA,
+        BW,
+        BV,
+        BR,
+        IO,
+        BN,
+        BG,
+        BF,
+        BI,
+        KH,
+        CM,
+        CA,
+        CV,
+        KY,
+        CF,
+        TD,
+        CL,
+        CN,
+        CX,
+        CC,
+        CO,
+        KM,
+        CG,
+        CD,
+        CK,
+        CR,
+        CI,
+        HR,
+        CU,
+        CW,
+        CY,
+        CZ,
+        DK,
+        DJ,
+        DM,
+        DO,
+        EC,
+        EG,
+        SV,
+        GQ,
+        ER,
+        EE,
+        ET,
+        FK,
+        FO,
+        FJ,
+        FI,
+        FR,
+        GF,
+        PF,
+        TF,
+        GA,
+        GM,
+        GE,
+        DE,
+        GH,
+        GI,
+        GR,
+        GL,
+        GD,
+        GP,
+        GU,
+        GT,
+        GG,
+        GN,
+        GW,
+        GY,
+        HT,
+        HM,
+        VA,
+        HN,
+        HK,
+        HU,
+        IS,
+        IN,
+        ID,
+        IR,
+        IQ,
+        IE,
+        IM,
+        IL,
+        IT,
+        JM,
+        JP,
+        JE,
+        JO,
+        KZ,
+        KE,
+        KI,
+        KP,
+        KR,
+        KW,
+        KG,
+        LA,
+        LV,
+        LB,
+        LS,
+        LR,
+        LY,
+        LI,
+        LT,
+        LU,
+        MO,
+        MK,
+        MG,
+        MW,
+        MY,
+        MV,
+        ML,
+        MT,
+        MH,
+        MQ,
+        MR,
+        MU,
+        YT,
+        MX,
+        FM,
+        MD,
+        MC,
+        MN,
+        ME,
+        MS,
+        MA,
+        MZ,
+        MM,
+        NA,
+        NR,
+        NP,
+        NL,
+        NC,
+        NZ,
+        NI,
+        NE,
+        NG,
+        NU,
+        NF,
+        MP,
+        NO,
+        OM,
+        PK,
+        PW,
+        PS,
+        PA,
+        PG,
+        PY,
+        PE,
+        PH,
+        PN,
+        PL,
+        PT,
+        PR,
+        QA,
+        RE,
+        RO,
+        RU,
+        RW,
+        BL,
+        SH,
+        KN,
+        LC,
+        MF,
+        PM,
+        VC,
+        WS,
+        SM,
+        ST,
+        SA,
+        SN,
+        RS,
+        SC,
+        SL,
+        SG,
+        SX,
+        SK,
+        SI,
+        SB,
+        SO,
+        ZA,
+        GS,
+        SS,
+        ES,
+        LK,
+        SD,
+        SR,
+        SJ,
+        SZ,
+        SE,
+        CH,
+        SY,
+        TW,
+        TJ,
+        TZ,
+        TH,
+        TL,
+        TG,
+        TK,
+        TO,
+        TT,
+        TN,
+        TR,
+        TM,
+        TC,
+        TV,
+        UG,
+        UA,
+        AE,
+        GB,
+        US,
+        UM,
+        UY,
+        UZ,
+        VU,
+        VE,
+        VN,
+        VG,
+        VI,
+        WF,
+        EH,
+        YE,
+        ZM,
+        ZW
+        `, {
+            method: 'GET',
+            headers: {
+                "x-rapidapi-host": "spott.p.rapidapi.com",
+                "x-rapidapi-key": "2b140728d4msh9fed995ffd67651p16455cjsnd6c6d9e20a66",
+            }
+        });
 
-    // console.log('filter', location[1])
+        const data = await response.json()
+        if (data.length < 1) {
+            return console.log('not found')
+        }
+        console.log('place', data[0].name)
+
+        setLocation(data[0].name)
+    }
+
+    const handleLocation = async () => {
+        const response = await fetch(`https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&address=${location}`, {
+            method: 'GET',
+            headers: {
+                "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
+                "x-rapidapi-key": "2b140728d4msh9fed995ffd67651p16455cjsnd6c6d9e20a66"
+            }
+        });
+
+        const data = await response.json()
+        if (data.length < 1) {
+            return console.log('not found')
+        }
+        setLat(data.results[0].geometry.location.lat);
+        setLng(data.results[0].geometry.location.lng);
+    }
 
     const handleSearch = async () => {
         try {
-            const response = await fetch(`https://trueway-places.p.rapidapi.com/FindPlacesNearby?type=hospital&radius=${radius}&language=en&location=6.5244,3.3792`, {
+            const response = await fetch(`https://trueway-places.p.rapidapi.com/FindPlacesNearby?type=hospital&radius=${radius}&language=en&location=${lat},${lng}`, {
                 method: 'GET',
                 headers: {
                     "x-rapidapi-host": "trueway-places.p.rapidapi.com",
@@ -55,8 +342,10 @@ const Map = () => {
                 }
             })
             const data = await response.json();
-            console.log(data)
-            setResult(data.results)
+            console.log('results', data)
+            if (!data.results) {
+                return console.log('not found')
+            }
             return data;
         }
         catch (e) {
@@ -65,31 +354,63 @@ const Map = () => {
 
     }
 
-    console.log('result', result)
     return (
-        <div style={{marginTop: '3em'}}>
-            <div>
-                <Col span={12}>
-                    {/* <Input placeholder="search by location" allowClear onChange={e => setSearchKey(e.target.value)} /> */}
-                    <InputNumber min={1} max={100} defaultValue={12} onChange={(e: any) => setRadius(e)} />,
-                </Col>
+        <div style={{ marginTop: '3em' }}>
+            <div className={classes.flex}>
+                <TextField
+                    id="outlined-number"
+                    defaultValue='12'
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    className={classes.root}
+                    variant="outlined"
+                    onChange={e => {
+                        setRadius(e.target.value);
+                        console.log(e.target.value)
+                    }}
+                />
+                <Paper component="form" className={classes.root}>
+
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Search Hospitals By Location"
+                        onChange={e => {
+                            setSearchKey(e.target.value)
+                            handlePlace();
+                        }}
+                        inputProps={{ 'aria-label': 'search google maps' }}
+                    />
+                    <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={e => {
+                        e.preventDefault();
+                        handleLocation();
+                        handleSearch();
+                    }}>
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
             </div>
             <div>
-                <h1>Search Result</h1>
-                {result.length < 1 ? 
-                <div>Loading ...</div> : 
-                result.map(data => (
-                    <div key={data.id}>
-                        <Descriptions  layout="vertical" bordered>
-                            <Descriptions.Item label="Address">{data.address}</Descriptions.Item>
-                            <Descriptions.Item label="Hospital Name">{data.name}</Descriptions.Item>
-                            <Descriptions.Item label="Phone">{data.phone_number}</Descriptions.Item>
-                        </Descriptions>
-                    </div>
-                ))}
+                <p>Hospitals are represented with the <AddCircleOutlineRoundedIcon color="secondary" /> icon on the map </p>
+            </div>
+            <div >
+                <Map style={{ height: '35em', width: '100%' }} center={[lat, lng]} zoom={15}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {/* {positionA.map(pos => (
+            <Marker position={[pos, pos]} icon={icon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        ))} */}
+                </Map>
             </div>
         </div>
     )
 }
 
-export default Map;
+export default Maps;
